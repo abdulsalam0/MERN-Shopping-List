b7fcdac44376669819ba57e6e6587e3adb8d4e0a
abdulsalam00 <aboubakar123678@gmail.com>
2020-03-24 13:00:00 +0000

    add client react page / add navbar

---
+++ client/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import AppNavbar from "./components/AppNavbar";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <AppNavbar/>
+      <div className="container">
+      <h1>hello world</h1>
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
